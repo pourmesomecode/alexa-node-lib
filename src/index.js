1dffb357e5298c971e4f822e6d0927f5520da520
pourmesomecode <hello@pourmesomecode.com>
2017-07-22 20:40:51 +0100

    Add inital commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+import validation from './lib/validation';
+
+const server = express();
+const alexaNode = {};
+
+alexaNode.app = (name, skillID, port) => {
+  server.post('/', validation, (req, res) => {
+    console.log(res, '<<<<<');
+  });
+
+  server.listen(process.env.PORT || port, () => {
+    console.log(`Server currently running on port ${port}`);
+  });
+};
+
+alexaNode.intent = () => {
+
+};
+
+alexaNode.slot = () => {
+
+};
+
+alexaNode.response = () => {
+
+};
